d5f4c69c5b90094fc2fb6b1b50ce88d741ae6b3e
sachabate <31642615+sachabate@users.noreply.github.com>
2023-03-26 23:30:49 -0600

    feat: add config for rose-pine theme

---
+++ after/plugin/rose-pine.lua
@@ -0,0 +1,14 @@
+require('rose-pine').setup({
+	--- @usage 'auto'|'main'|'moon'|'dawn'
+	variant = 'moon',
+	--- @usage 'main'|'moon'|'dawn'
+	dark_variant = 'main',
+	bold_vert_split = false,
+	dim_nc_background = false,
+	disable_background = false,
+	disable_float_background = false,
+	disable_italics = false,
+})
+
+-- Set colorscheme after options
+vim.cmd('colorscheme rose-pine')
--- lua/sacha/packer.lua
+++ lua/sacha/packer.lua
@@ -11,8 +11,6 @@ return require('packer').startup(function(use)
 
   use({ 'rose-pine/neovim', as = 'rose-pine' })
 
-  vim.cmd('colorscheme rose-pine')
-
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
