ef80c6f3a3d8d20f0d3011cfc5004ec3bac2a443
sachabate <abate@fieldsafesolutions.com>
2023-03-31 16:25:33 -0600

    feat: add telescope preview config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,10 @@
+require('telescope').setup({
+    defaults = {
+        layout_config = { flex = {flip_columns = 120} },
+        layout_strategy = 'flex',
+    }
+})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function()
