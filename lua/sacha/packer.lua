dac6f1796faf15721c207dae26ef144932d84652
sachabate <31642615+sachabate@users.noreply.github.com>
2023-04-01 15:43:03 -0600

    feat: add treesitter-context

--- lua/sacha/packer.lua
+++ lua/sacha/packer.lua
@@ -1,4 +1,15 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
@@ -12,6 +23,7 @@ return require('packer').startup(function(use)
     use({ 'rose-pine/neovim', as = 'rose-pine' })
 
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter-context')
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
@@ -39,4 +51,7 @@ return require('packer').startup(function(use)
         }
     }
 
+    if packer_bootstrap then 
+        require('packer').sync()
+    end
 end)
