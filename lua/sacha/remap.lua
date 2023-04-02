4bec5bc9a9698604bf0470f66f38060ae354492f
sachabate <31642615+sachabate@users.noreply.github.com>
2023-04-02 17:42:22 -0600

    feat: remap navigation bindings

--- lua/sacha/remap.lua
+++ lua/sacha/remap.lua
@@ -7,13 +7,18 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("i", "<A-o>", "<Esc>o")
 vim.keymap.set("i", "<A-O>", "<Esc>O")
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-n>", "<C-d>zz")
+vim.keymap.set("n", "<C-p>", "<C-u>zz")
+
+vim.keymap.set("n", "<C-h>", "<C-W>h")
+vim.keymap.set("n", "<C-j>", "<C-W>j")
+vim.keymap.set("n", "<C-k>", "<C-W>k")
+vim.keymap.set("n", "<C-l>", "<C-W>l")
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<A-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<A-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
