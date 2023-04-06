d14e2a8cdf0bf147e069c2b07cfe84f04db7ea75
sachabate <abate@fieldsafesolutions.com>
2023-04-06 15:15:00 -0600

    feat: tweak nav keybinds and search/scroll preferences

--- lua/sacha/set.lua
+++ lua/sacha/set.lua
@@ -14,12 +14,12 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
