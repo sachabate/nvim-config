b2e39b5dda2a70ce26ec6446d227a8044ac01f5b
sachabate <31642615+sachabate@users.noreply.github.com>
2023-04-07 13:16:19 -0600

    feat: add vim-commentary

--- lua/sacha/packer.lua
+++ lua/sacha/packer.lua
@@ -37,6 +37,7 @@ return require('packer').startup(function(use)
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
+    use('tpope/vim-commentary')
 
     use {
         'VonHeikemen/lsp-zero.nvim',
