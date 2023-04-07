f9ee4b7ed21e8c454d1aaf331117b2b37c4035d8
sachabate <31642615+sachabate@users.noreply.github.com>
2023-04-07 13:52:47 -0600

    feat: add telescope picker bindings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,6 +7,13 @@ require('telescope').setup({
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pr', builtin.oldfiles, {})
+
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>pt', builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>pc', builtin.colorscheme, {})
