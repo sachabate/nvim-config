74ef48702cfee4a583192733dde1d346e554b2e3
sachabate <abate@fieldsafesolutions.com>
2023-04-11 12:30:29 -0600

    feat: add supertabbing bindings for luasnip

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,19 +3,30 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-	"tsserver",
-	"pyright",
+    "tsserver",
+    "pyright",
 })
 
+require("lspconfig").sourcekit.setup{}
+
 local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
 })
 
 lsp.setup()
-
-require("lspconfig").sourcekit.setup{}
+cmp.setup({
+    sources = {
+        {name = 'nvim_lsp'},
+        {name = 'luasnip'},
+    },
+    mapping = {
+        ['<Tab>'] = cmp_action.luasnip_supertab(),
+        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    },
+})
