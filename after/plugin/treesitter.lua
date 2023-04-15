64c69f9b1f24188a5420f83a8adc8231644e0e5a
SachaBate <31642615+sachabate@users.noreply.github.com>
2023-04-15 10:02:30 -0600

    fix: remove treesitter help

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "astro", "svelte", "vue", "html", "python", "elixir", "php", "rust", "c", "scss", "markdown_inline", "json", "lua", "vim", "help", "query" },
+  ensure_installed = { "javascript", "typescript", "astro", "svelte", "vue", "html", "python", "elixir", "php", "rust", "c", "scss", "markdown_inline", "json", "lua", "vim", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
